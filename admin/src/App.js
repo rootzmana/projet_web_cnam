dc3ad2893c52b9891c0d3033ccc9c9f3e1eb5602
Matthieu Desvignes <Matthieu.Desvignes@saint-gobain.com>
2020-05-04 16:06:37 +0200

    Setup basic React Admin

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { HydraAdmin } from "@api-platform/admin";
+import { Admin } from "react-admin";
+import jsonServerProvider from "ra-data-json-server";
 
-// Replace with your own API entrypoint
-// For instance if https://example.com/api/books is the path to the collection of book resources, then the entrypoint is https://example.com/api
-export default () => (
-  <HydraAdmin entrypoint={process.env.REACT_APP_API_ENDPOINT} />
-);
+const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
+const App = () => <Admin dataProvider={dataProvider} />;
+
+export default App;
